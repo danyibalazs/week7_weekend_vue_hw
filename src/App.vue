651b1e195ceb7ae61bea38fb21a09904061c4238
danyibalazs <danyi.balage@gmail.com>
2020-02-29 14:08:36 +0000

    joke api/component added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <joke-component :joke='joke'></joke-component> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JokeComponent from './components/JokeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'joke-component': JokeComponent
+  },
+  data(){
+    return {
+      joke: {}
+    }
+  },
+  mounted(){
+    fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+    .then(response => response.json())
+    .then(jokeData => {
+      this.joke = jokeData[0];
+
+    })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/JokeComponent.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <p>{{joke.setup}}</p>
+    <p>{{joke.punchline}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'joke-component',
+  props: ['joke']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
